2e679145875f45aa1122d6612f669bee7612f6a8
Zoldur <36329578+zoldur@users.noreply.github.com>
2018-02-12 23:46:39 +0000

    Update cropcoin.sh

--- cropcoin.sh
+++ cropcoin.sh
@@ -24,7 +24,8 @@ echo -e "Installing required packages, it may take some time to finish.${NC}"
 apt-get update > /dev/null 2>&1
 apt-get install -y software-properties-common build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
 libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git wget pwgen curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev lzip > /dev/null 2>&1
+bsdmainutils libdb4.8++-dev libminiupnpc-dev lzip
+clear
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
